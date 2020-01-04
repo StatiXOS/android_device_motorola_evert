0e035b0e8d2131bcddb46a8205cd00bb000acfdd
Sourajit Karmakar <45220673+sourajitk@users.noreply.github.com>
2020-01-04 18:20:33 -0500

    evert: Drop LineageOS Touch HAL.

--- device.mk
+++ device.mk
@@ -65,10 +65,6 @@ PRODUCT_BOOT_JARS += qcom.fmradio
 PRODUCT_PACKAGES += \
     init.evert.rc
 
-# Lineage hardware
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.sdm660
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
